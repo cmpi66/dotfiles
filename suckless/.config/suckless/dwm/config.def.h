6294607a1e9c831ddac92e15e89719ee844ceff5
chris <munozchris166@yahoo.com>
2022-07-01 18:26:14 -0400

    autopush

--- suckless/.config/suckless/dwm/config.def.h
+++ suckless/.config/suckless/dwm/config.def.h
@@ -85,7 +85,7 @@ static Sp scratchpads[] = {
 
 /* tagging */
 // static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = { " 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9" };
+static const char *tags[] = { " 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9" };
 
 static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
 static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
