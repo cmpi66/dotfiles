098fb6101c53777e894cbd848cca3fa4deb654d9
Chris <chris@gentoobox.com>
2022-09-15 17:38:42 -0400

    kj escape fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -117,7 +117,8 @@ bind -M insert \ck history-search-backward # Up
 bind -M insert \cj history-search-forward # Down
 bind -M insert \cn 'ncdu'
  # bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
- bind -M insert -m default jk backward-char force-repaint
+bind -M insert -m default jk backward-char force-repaint
+bind -M insert -m default kj backward-char force-repaint
 
 # Custom keybindings default mode
 bind \co 'lf'
