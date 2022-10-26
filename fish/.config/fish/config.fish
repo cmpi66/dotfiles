4c65649c0133b25e2e71e03985a0fe1a561d03a0
chris <munozchris166@yahoo.com>
2022-10-26 15:25:13 -0400

    moving fonts to pr

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -129,7 +129,7 @@ bind \ck history-search-backward # Up
 bind \cj history-search-forward # Down
 bind \cn 'ncdu'
 
-# neofetch
+neofetch
 # colorscript random
 zoxide init fish | source
 starship init fish | source
