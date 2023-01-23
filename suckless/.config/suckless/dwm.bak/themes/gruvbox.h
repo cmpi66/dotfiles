b014b936258f97ca139f4f3ac6c5e7da93fca4c7
Chris <munozchris@posteo.net>
2023-01-22 19:07:17 -0500

    autopush

---
+++ suckless/.config/suckless/dwm.bak/themes/gruvbox.h
@@ -0,0 +1,5 @@
+static const char col_gray1[]       = "#1d2021";
+static const char col_gray2[]       = "#d79921";
+static const char col_gray3[]       = "#ebdbb2";
+static const char col_gray4[]       = "#ebdbb2";
+static const char col_cyan[]        = "#458588";
