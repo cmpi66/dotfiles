5c99331beffbce2bff9610f91312bd66581b64a1
chris <munozchris@posteo.net>
2023-02-02 08:25:07 -0500

    updates

--- suckless/.config/suckless/slstatus/themes/catppuccin.h
+++ suckless/.config/suckless/slstatus/themes/catppuccin.h
@@ -2,28 +2,28 @@ static const struct arg args[] = {
 	/* function format          argument */
     // {kernel_release, "^c#abe9b3^  %s  ", NULL },
     //{run_command,  "^c#abe9b3^  %s ",      "sb-price.sh" },
-    {run_command,  "^c#96cdfb^ ^b#161320^ %2s ",     "mail.sh" },
-    {run_command,  "^c#e8a2af^ ^b#161320^ %2s ",     "pacpackages.sh" },
-    {run_command,  "^c#fae380^ ^b#161320^ %s ",      "battery.sh" },
+    {run_command,  "^c#96cdfb^ ^b#1e1e2e^ %2s ",     "mail.sh" },
+    {run_command,  "^c#e8a2af^ ^b#1e1e2e^ %2s ",     "pacpackages.sh" },
+    {run_command,  "^c#fae380^ ^b#1e1e2e^ %s ",      "battery.sh" },
     //{run_command,  "^c#e8a2af^  %2s ",     "sb-forecast.sh" },
    {run_command,  "^c#abe9b3^  %2s ",     "internet.sh" },
-    {uptime,   "^c#c9cbff^ ^b#161320^  %s  ", NULL },
-    {cpu_perc, "^c#ddb6f2^ ^b#161320^  %s%%  ", NULL },
-    {ram_used, "^c#74c7ec^ ^b#161320^  %s  ", NULL },
-    {temp,     "^c#f28fad^ ^b#161320^  %s  ",    "/sys/class/thermal/thermal_zone8/temp" },
+    {uptime,   "^c#c9cbff^ ^b#1e1e2e^  %s  ", NULL },
+    {cpu_perc, "^c#ddb6f2^ ^b#1e1e2e^  %s%%  ", NULL },
+    {ram_used, "^c#74c7ec^ ^b#1e1e2e^  %s  ", NULL },
+    {temp,     "^c#f28fad^ ^b#1e1e2e^  %s  ",    "/sys/class/thermal/thermal_zone8/temp" },
     //{run_command,"^c#fae380^  %2s    ",   "pacman -Qu | grep -Fcv '[ignored]'" },
     //{run_command,  "^c#fae380^  %2s",     "sb-pacpackages.sh" },
     //{run_command,  "^c#96cdfb^  %s",      "sb-battery.sh" },
     // { run_command, " %4s  ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
 
 	//{wifi_perc, "  %s%% | ", "wlp0s20f" }
-	{disk_free, "^c#f2cdcd^ ^b#161320^  %s  " , "/" },
+	{disk_free, "^c#f2cdcd^ ^b#1e1e2e^  %s  " , "/" },
 	//{disk_total, "%s   " , "/" },
 	//{battery_state, "^c#96cdfb^ %s " , "BAT0" },
         //{battery_perc,  "%s%%  ",  "BAT0" },
-	// { datetime, "%s",   "^c#f8bd96^ ^b#161320^  %r   " },
+	// { datetime, "%s",   "^c#f8bd96^ ^b#1e1e2e^  %r   " },
 	{ datetime, "%s",   "^c#f8bd96^  %a %b %d   %r   " },
-    {run_command,  "^c#74c7Ec^  ^b#161320^ %2s ",      "music.sh" },
+    {run_command,  "^c#74c7Ec^  ^b#1e1e2e^ %2s ",      "music.sh" },
 
 	
 };
