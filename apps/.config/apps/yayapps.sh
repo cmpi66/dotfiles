59e80db75cc8ae3a207892e21e070fe23aae0ac9
chris <munozchris@posteo.net>
2023-02-22 18:41:41 -0500

    autopush

--- apps/.config/apps/yayapps.sh
+++ apps/.config/apps/yayapps.sh
@@ -5,24 +5,24 @@
 yay --noconfirm --needed -S \
   7-zip-full \
   abook-configdir \
-  ani-cli \
-  anki \
+  # ani-cli \
+  # anki \
   aur/trayer-srg \
-  catppuccin-gtk-theme-macchiato \
-  etcher-bin \
-  hakuneko-desktop \
-  htop-vim \
-  i3lock-color \
-  kcc \
-  kjv-apocrypha \
+  # catppuccin-gtk-theme-macchiato \
+  # etcher-bin \
+  # hakuneko-desktop \
+  # htop-vim \
+  # i3lock-color \
+  # kcc \
+  # kjv-apocrypha \
   lf \
   mutt-wizard \
   noto-color-emoji-fontconfig \
   opustags \
-  rpi-imager-bin \
+  # rpi-imager-bin \
   sc-im \
   task-spooler \
-  timeshift \
+  # timeshift \
   tremc \
   ueberzug \
   update-grub \
