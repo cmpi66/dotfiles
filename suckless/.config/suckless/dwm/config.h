f74c0e2c0d66b33a3a4f8754256c71214415a003
chris <munozchris@posteo.net>
2023-04-29 09:09:22 -0400

    updates, deleting and moving scripts

--- suckless/.config/suckless/dwm/config.h
+++ suckless/.config/suckless/dwm/config.h
@@ -217,7 +217,7 @@ static Key keys[] = {
 
 	{ MODKEY,		          XK_p,	spawn,		{.v = (const char*[]){ "mpc", "toggle",  NULL } } },
 
-	{ MODKEY|ShiftMask,	   XK_p,	spawn,		{.v = (const char*[]){ "mpc", "stop",  NULL } } },
+	{ MODKEY|ShiftMask,	   XK_p,	spawn,		SHCMD("mpc pause; pauseallmpv") },
   { 0,				XK_Print,	spawn,		SHCMD("maim pic-full-$(date '+%y%m%d-%H%M-%S').png") },
   { ShiftMask,		               XK_Print,	spawn,		{.v = (const char*[]){ "maimpic", NULL } } },
   { MODKEY,		                 	 XK_Print,	spawn,		{.v = (const char*[]){ "dmenurecord", NULL } } },
