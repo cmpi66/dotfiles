82a0ee57160a415acf0772a0021f2b408317f0a1
Chris <munozchris@posteo.net>
2023-05-07 17:12:29 -0400

    updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -127,7 +127,8 @@ abbr reboot "doas openrc-shutdown -r now"
 abbr mpv "mpv --fullscreen"
 abbr lynx "lynxub"
 abbr se "br --conf ~/.config/broot/open.hjson ~/.local/bin/"
-
+abbr syncartilces "rclone sync -v  --transfers 20 --retries 5 --s3-upload-cutoff=0 --exclude '*.md'  ~/docs/md/cmm-articles/  Idrive:cmmarketer/articles"
+abbr syncsamples "rclone sync -v --transfers 20 --retries 5 --s3-upload-cutoff=0 --exclude '*.md'  ~/copywriting/samples/final/ Idrive:cmmarketer/samples"
 ### ANSIBLE
 
 
