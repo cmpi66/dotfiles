39a11bca2d90caf546a4806cec8edbb952609a15
Chris <munozchris@posteo.net>
2023-05-18 10:51:19 -0400

    updates

--- suckless/.config/suckless/dwm/config.h
+++ suckless/.config/suckless/dwm/config.h
@@ -227,6 +227,7 @@ static Key keys[] = {
   // { MODKEY,			XK_F11,		spawn,		SHCMD("mpv --untimed --no-cache --no-osc --no-input-default-bindings --profile=low-latency --input-conf=/dev/null --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)") },
   { MODKEY,			                  XK_F9,		 spawn,		      SHCMD("dmenumount") },
   { MODKEY,			                  XK_F10,		 spawn,		      SHCMD("dmenuunmount") },
+  { MODKEY,			                  XK_F11,		 spawn,		      SHCMD("otp") },
   { MODKEY,			                  XK_F12,		 spawn,		      SHCMD("remaps") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
--- zsh/.config/zsh/.zshenv
+++ zsh/.config/zsh/.zshenv
@@ -1,4 +1,4 @@
 export ZDOTDIR=$HOME/.config/zsh   
-# export GPG_TTY=$(tty)
-# export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-# gpgconf --launch gpg-agent
+# export GPG_TTY=$(tty) # apparently this line isn't needed it works either way here. On arch this line hinders the pinentry program 
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+gpgconf --launch gpg-agent
