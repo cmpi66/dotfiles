08ada57cdf9ac8646a98b416e9ac6346228e4836
chris <munozchris@posteo.net>
2023-06-08 18:34:27 -0400

    autopush

--- apps/.config/apps/yayapps.sh
+++ apps/.config/apps/yayapps.sh
@@ -3,23 +3,17 @@
 #aur apps 
 
 yay --noconfirm --needed -S \
-  7-zip-full \
   abook\
-  ani-cli \
   aur/trayer-srg \
-  hakuneko-desktop \
   pam-gnupg \
   htop-vim \
   i3lock-color \
-  kcc \
   lf \
   mutt-wizard-git \
   opustags \
-  rpi-imager-bin \
   sc-im \
   task-spooler \
   stig-git \
-  upwork \
   urlview-xdg-git \
   xidlehook \
   librewolf-bin \
