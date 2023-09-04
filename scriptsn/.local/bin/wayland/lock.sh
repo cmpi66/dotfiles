cd50ab155cf55f2df27b8e8b9e9769b9a397e0a4
Chris <munozchris@posteo.net>
2023-09-04 16:07:02 -0400

    updates

--- scriptsn/.local/bin/wayland/lock.sh
+++ scriptsn/.local/bin/wayland/lock.sh
@@ -6,7 +6,6 @@ pkill swayidle
 
 
 swayidle -w \
-    timeout 300 'swaylock -e -F -C /home/chris/.config/swaylock/config -i /home/chris/.local/bin/2770299.png' \
-         resume 'hyprctl dispatch dpms on"' \
+    timeout 600 'swaylock -e -F -C /home/chris/.config/swaylock/config -i /home/chris/.local/bin/2770299.png' 
 
 
