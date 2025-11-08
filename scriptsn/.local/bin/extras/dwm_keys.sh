84aa0439c4ea8ba9dee1b876f75161c3a5a9807a
chris <munozchris@posteo.net>
2025-11-08 16:40:58 -0500

    updates

--- scriptsn/.local/bin/extras/dwm_keys.sh
+++
@@ -1,4 +0,0 @@
-sed -n '/start_keys/,/end_keys/p' ~/.config/keys.h | \
-  grep -v '//' | \
-  sed -e 's/^[ \t]*//' | \
- yad --text-info --back=#282a36 --fore=#8be9fd --geometry=800x1000 --no-buttons --fontname="JetbrainsMono Bold 11"
