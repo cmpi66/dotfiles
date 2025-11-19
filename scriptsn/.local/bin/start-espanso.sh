26c3f1e64042902e801647669f86e18f8d48a12e
chris <munozchris@posteo.net>
2025-11-19 13:18:53 -0500

    updates

--- scriptsn/.local/bin/start-espanso.sh
+++ scriptsn/.local/bin/start-espanso.sh
@@ -3,8 +3,8 @@
 # If espanso daemon is running, will ask to kill, else will ask to start.
 
 if pidof espanso >/dev/null; then
-  [ "$(printf "No\\nYes" | rofi -dmenu -i -p "Turn off espanso daemon?")" = "Yes" ] && killall espanso && notify-send "espanso daemon disabled."
+  [ "$(printf "No\\nYes" | rofi -dmenu -i -l 2 -p "Turn off espanso daemon?")" = "Yes" ] && killall espanso && notify-send "espanso daemon disabled."
 else
   ifinstalled espanso || exit
-  [ "$(printf "No\\nYes" | rofi -dmenu -i -p "Turn on espanso daemon?")" = "Yes" ] && espanso service start --unmanaged && notify-send "espanso daemon enabled."
+  [ "$(printf "No\\nYes" | rofi -dmenu -i -l 2 -p "Turn on espanso daemon?")" = "Yes" ] && espanso service start --unmanaged && notify-send "espanso daemon enabled."
 fi
