ee6d40109801b2b157360ddd5e184cbe5c8afd48
chris <munozchris@posteo.net>
2025-11-19 13:21:34 -0500

    updates

--- gpg-failure-fix-temp.sh
+++ gpg-failure-fix-temp.sh
@@ -4,6 +4,5 @@ sudo rc-service pcscd restart
 gpgconf --kill all
 killall -9 gpg-agent scdaemon || true
 
-export GPG_TTY=$(tty)
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 gpgconf --launch gpg-agent
