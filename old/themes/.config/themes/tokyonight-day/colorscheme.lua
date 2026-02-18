fb539fe4edc3e15bdb43da3b77e2d63683a16ce1
chris <munozchris@posteo.net>
2026-02-18 13:53:09 -0500

    clean up

---
+++ old/themes/.config/themes/tokyonight-day/colorscheme.lua
@@ -0,0 +1,9 @@
+-- File: ~/.config/nvim/lua/plugins/tokyonight.lua
+return {
+	"folke/tokyonight.nvim",
+	opts = {
+		style = "day", -- Options: "storm", "moon", "night", "day"
+		transparent = false, -- optional
+		terminal_colors = true,
+	},
+}
--- themes/.config/themes/tokyonight-day/colorscheme.lua
+++
@@ -1,9 +0,0 @@
--- File: ~/.config/nvim/lua/plugins/tokyonight.lua
-return {
-	"folke/tokyonight.nvim",
-	opts = {
-		style = "day", -- Options: "storm", "moon", "night", "day"
-		transparent = false, -- optional
-		terminal_colors = true,
-	},
-}
