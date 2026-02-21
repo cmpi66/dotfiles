09ff819a37e94c4840a629447a94f7049708c9b9
chris <munozchris@posteo.net>
2026-02-21 17:12:44 -0500

    add rofi system menu for one-off utils and swap keybindings for it and passmenu as well as adding some one off scripts

--- scriptsn/.local/bin/find-music.sh
+++ scriptsn/.local/bin/find-music.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-file=$(fd -e .flac -e .mp3 -e .m4a -e .opus . ~/.local/share/music -p -t f | sort | sed "s|^/home/chris/.local/share/music/||" | rofi -dmenu -i -l 1) || exit 0
+file=$(fd -e .flac -e .mp3 -e .m4a -e .opus . ~/.local/share/music -p -t f | sort | sed "s|^/home/chris/.local/share/music/||" | rofi -dmenu -theme findmusic.rasi -i -l 1) || exit 0
 #
-notify-send "Playing $file" && mpc insert "$file" && mpc next >/dev/null && mpc play >/dev/null
+notify-send "🎶 Playing $file" && mpc insert "$file" && mpc next >/dev/null && mpc play >/dev/null
