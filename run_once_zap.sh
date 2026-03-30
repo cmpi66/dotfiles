83cd1823ed89b683415c076e196d6b52667d5432
chris <munozchris@posteo.net>
2026-03-30 18:12:25 -0400

    small refactor and begining to add templates and ignores

--- run_once_zap.sh
+++ run_once_zap.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 # Install zap zsh plugin manager
-zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 --keep
+
+if [ ! -d "$HOME/.local/share/zap/" ]; then
+  echo "Getting zap.sh"
+  zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 --keep
+else
+  echo "zap zsh already found, skipping."
+fi
