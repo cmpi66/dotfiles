3841f71659f847ac748a01d23b0be964b87a86f9
chris <munozchris@posteo.net>
2026-04-10 13:47:37 -0400

    updates and changing to age encryption over gpg

--- dot_local/bin/executable_pass-cache.sh
+++ dot_local/bin/executable_pass-cache.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-CACHE="${XDG_RUNTIME_DIR:-/tmp}/aerc-pass-$USER-$1"
+ENTRY="$1"
+CACHE_BASE="${XDG_RUNTIME_DIR:-/tmp}/aerc-pass-$USER"
+CACHE="$CACHE_BASE/$ENTRY"
 
 # expire after 1 hour
 TTL=3600
 
+umask 077
+mkdir -p "$(dirname "$CACHE")"
+
 if [ -f "$CACHE" ]; then
-  if [ $(($(date +%s) - $(stat -c %Y "$CACHE"))) -lt $TTL ]; then
+  if [ $(($(date +%s) - $(stat -c %Y "$CACHE"))) -lt "$TTL" ]; then
     cat "$CACHE"
     exit 0
   fi
 fi
 
-pass show "$1" | tee "$CACHE"
+pass show "$ENTRY" | tee "$CACHE"
