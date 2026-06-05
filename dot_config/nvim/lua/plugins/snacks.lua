9dd7f594028b03f6fbfbfae72c2e21484b67cb28
chris <munozchris@posteo.net>
2026-06-05 11:57:15 -0400

    updates snacks for easier nav

---
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        formatters = {
+          file = {
+            filename_first = true,
+          },
+        },
+      },
+    },
+  },
+}
